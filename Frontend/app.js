3aef78f519e3d8cdd3fe534966424852ac9e1994
gilsonOrlando <gilson.quezada@unl.edu.ec>
2023-06-07 18:07:13 -0500

    TareaAxius

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,30 +1,29 @@
 new Vue({
-    el: '#app',
-    data: {
-      username: '',
-      password: ''
-    },
-    methods: {
-      submitForm() {
-        // Crea un objeto con los datos del formulario
-        const userData = {
-          username: this.username,
-          password: this.password
-        };
-  
-        // Realiza la solicitud POST a la API utilizando Axios
-        axios.post('URL_DE_TU_API', userData)
-          .then(response => {
-            // Maneja la respuesta de la API
-            console.log(response.data);
-            alert('Inicio de sesión exitoso');
-          })
-          .catch(error => {
-            // Maneja los errores de la solicitud
-            console.error(error);
-            alert('Error en el inicio de sesión');
-          });
-      }
+  el: '#app',
+  data: {
+    email: '',
+    password: ''
+  },
+  methods: {
+    submitForm() {
+      // Crea un objeto con los datos del formulario
+      const userData = {
+        email: this.email,
+        password: this.password
+      };
+
+      // Realiza la solicitud POST a la API utilizando Axios
+      axios.post('URL_DE_TU_API', null, { params: userData })
+        .then(response => {
+          // Maneja la respuesta de la API
+          console.log(response.data);
+          alert('Inicio de sesión exitoso');
+        })
+        .catch(error => {
+          // Maneja los errores de la solicitud
+          console.error(error);
+          alert('Error en el inicio de sesión');
+        });
     }
-  });
-  
+  }
+});
