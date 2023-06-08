21be8033a651b6286ba9a4a513cd1b86f135d28f
Jorge <jorge.poma@unl.edu.ec>
2023-06-07 23:49:08 -0500

    fix frontend

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,8 +1,10 @@
 new Vue({
   el: '#app',
-  data: {
-    email: '',
-    password: ''
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
   },
   methods: {
     submitForm: function() {
@@ -13,12 +15,13 @@ new Vue({
       };
 
       // Realizar la solicitud POST a la API
-      axios.post('http://localhost:3000/auth/login', data)
+      axios.post('http://127.0.0.1:3000/auth/login', data)
         .then(response => {
           // Comprobar el resultado del inicio de sesión
           if (response.data.success) {
             alert('Inicio de sesión exitoso');
           } else {
+            console.log(response)
             alert('Inicio de sesión incorrecto');
           }
         })
