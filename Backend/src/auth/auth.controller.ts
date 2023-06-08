e97798ad675b8320c55ea337bcf9c112c8740b8d
Jorge <jorge.poma@unl.edu.ec>
2023-06-07 23:24:14 -0500

    fix frontend - enable cors

--- Backend/src/auth/auth.controller.ts
+++ Backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -9,26 +9,14 @@ export class AuthController {
 
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto.correo, createAuthDto.contraseña);
+    return this.authService.create(
+      createAuthDto.correo,
+      createAuthDto.contraseña,
+    );
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('login')
+  login(@Body() userObjectLogin: CreateAuthDto) {
+    return this.authService.login(userObjectLogin);
   }
 }
