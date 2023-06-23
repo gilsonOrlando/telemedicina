077a7e4796f2037a2409098c90fa77a98af0c1a2
Jorge <jorge.poma@unl.edu.ec>
2023-06-23 15:04:06 -0500

    fix v.3

--- Frontend/client/src/components/Login.vue
+++ Frontend/client/src/components/Login.vue
@@ -1,91 +1,69 @@
 <template>
-    <div class="container">
-        <div class="triangle"></div>
-        <div class="div2">
-            <div id="app">
-                <div class="login-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="" class="bi bi-person-circle"
-                        viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd"
-                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg>
+    <section>
+        <v-row class="container">
+            <v-col class="triangle"></v-col>
+            <v-col class="column">
+                <!-- <div class="mx-4">
                     <h2 style="margin-top: -1px;">Iniciar sesión</h2>
-                    </div>
-                    <!-- <form @submit.prevent="submitForm" >
-              <div class="form-group" :class="{ 'form-group--error': $v.email.$error }">
-                <label for="email">Correo:</label>
-                <input type="email" id="email" v-model.trim.lazy="$v.email.$model">
-                <v-form>
-                    <v-text-field
-                        v-model="email"
-                        :rules="emailRules"
-                        label="Correo"
-                        required
-                    ></v-text-field>
-                    <v-text-field
-                        name="name"
-                        label="Enter your password"
-                        hint="At least 8 characters"
-                        min="8"
-                        :append-icon="value ? 'visibility' : 'visibility_off'"
-                        :append-icon-cb="() => (value = !value)"
-                        value="Password"
-                        :rules="[() => ('The email and password you entered don\'t match')]"
-                        error
-                        :type="value ? 'password' : 'text'"
-                    ></v-text-field>
-                </v-form>
-                <div class="error" v-if="!$v.email.required">Campo obligatorio.</div>
-                <div class="error" v-if="!$v.email.email && $v.email.$error">Correo invalido.</div>
-              </div>
-              <div class="form-group" :class="{ 'form-group--error': $v.password.$error }">
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" v-model.trim.lazy="$v.password.$model">
-                <div class="error" v-if="!$v.password.required">Campo obligatorio.</div>
-                <div class="error" v-if="!$v.password.regex && $v.password.$error">La contraseña debe tener entre 8 y 12 caracteres y contener almenos un numero, una letra mayuscula.</div>
-              </div>
-              <div class="form-group-button">
-                <label></label>
-                <button type="submit">Ingresar</button>
-              </div>
-            </form> -->
-                <!-- <form @submit.prevent="submitForm" mx-4> -->
-                    <div class="mx-4" >
-                        <!-- <label>email</label> -->
-                        
-                        <v-text-field v-model="form_login.email"
-                        placeholder=" email"
-                        :error-messages="form_login_email_errors"
+                </div>
+                <div class="mx-4">
+                    
+                    <v-text-field v-model="form_login.email" placeholder=" email" :error-messages="form_login_email_errors"
                         @input="$v.form_login.email.$touch()" @blur="$v.form_login.email.$touch()"></v-text-field>
-                    </div>
-                        <!-- <label>password</label> -->
-                    <div class="mx-4">
-                        <v-text-field v-model="form_login.password"
-                        placeholder="password" 
-                        :error-messages="form_login_password_errors" :counter="12"
-                        @input="$v.form_login.password.$touch()" @blur="$v.form_login.password.$touch()"></v-text-field>
-                    </div>
-                    <v-row>
 
-                        <v-col cols="6" lg="6" sm="6" md="4">
+
+                    <v-text-field v-model="form_login.password" placeholder="password"
+                        :error-messages="form_login_password_errors" :counter="12" @input="$v.form_login.password.$touch()" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'"
+                        @click:append="show = !show" @blur="$v.form_login.password.$touch()"></v-text-field>
+                </div>
+                <div class="mx-4" style="margin-top: 2em;">
+
+                    <v-btn class="pa-2" @click="submitForm">
+                        ingresar
+                    </v-btn>
+                    <v-btn class="pa-2" @click="clear">
+                        limpiar
+                    </v-btn>
+                </div> -->
+
+                <v-card class="card" max-width="500">
+                    <v-card-title>
+                        <center>
+                            <h2 style="margin-top: -1px;">Iniciar sesión</h2>
+                        </center>
+                    </v-card-title>
+                    <v-card-text>
+                        <br />
+                        <label>Correo</label>
+                        <v-text-field v-model="form_login.email" placeholder=" ingrese su correo"
+                            :error-messages="form_login_email_errors" @input="$v.form_login.email.$touch()"
+                            @blur="$v.form_login.email.$touch()"></v-text-field>
+                        <label>Contraseña</label>
+                        <v-text-field v-model="form_login.password" placeholder="password"
+                            :error-messages="form_login_password_errors" :counter="12"
+                            @input="$v.form_login.password.$touch()" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                            :type="show ? 'text' : 'password'" @click:append="show = !show"
+                            @blur="$v.form_login.password.$touch()"></v-text-field>
+                        <br />
+                        <center>
                             <v-btn class="pa-2" @click="submitForm">
                                 ingresar
                             </v-btn>
-                        </v-col>
-                        <v-col cols="6" lg="6" sm="6" md="4">
                             <v-btn class="pa-2" @click="clear">
                                 limpiar
                             </v-btn>
-                        </v-col>
-                    </v-row>
-                    <!-- </div> -->
-                    </div>
-                        
-                <!-- </form> -->
-                </div>
-            </div>
-        
+                            <br />
+                            <br />
+                            <label for="">¿Todavía no estas registrado?
+                                <a href="#">¡Inscribirse!</a> </label><br />
+                        </center>
+                    </v-card-text>
+                </v-card>
+
+            </v-col>
+            <!-- </form> -->
+        </v-row>
+    </section>
 </template>
   
 <script>
@@ -95,6 +73,7 @@ export default {
     name: 'App',
     data() {
         return {
+            show: false,
             form_login: {
                 email: null,
                 password: null
@@ -180,10 +159,10 @@ export default {
 }
 
 #app {
-    width: 100%;
     display: grid;
     justify-content: center;
     align-items: center;
+    align-content: center;
 }
 
 .login-container {
@@ -245,8 +224,8 @@ button:hover {
 .container {
     display: grid;
     position: relative;
-    grid-template-columns: 1fr 1fr 1fr;
-    width: 100vw;
+    grid-template-columns: 1fr 1fr;
+
     height: 100vh;
     padding: 0;
     margin: 0;
@@ -254,15 +233,17 @@ button:hover {
 
 .div2 {
     /* background-color: #edf1f2; */
-    width: 40vw;
     display: grid;
-    justify-items: center;
+    width: 100%;
+    justify-content: center;
     align-items: center;
 }
 
 .triangle {
     background: linear-gradient(-45deg, #5b84ac, #161e27);
     width: 60vw;
+    margin: 0;
+    padding: 0;
     transform: rotate(0deg);
     height: 100vh;
     clip-path: polygon(0% 0%, 100% 0%, 80% 100%, 0% 100%);
@@ -274,6 +255,8 @@ button:hover {
     background-repeat: no-repeat;
     background-position: center;
     content: "";
+    color: #161e27;
+    font-size: larger;
     position: fixed;
     width: 100%;
     height: 100%;
@@ -281,29 +264,55 @@ button:hover {
     opacity: 0.5;
 }
 
+.card {
+    justify-self: center;
+    align-self: center;
+}
+
+.column {
+        display: grid;
+        width: 40vw;
+        grid-template-columns: 1fr;
+        justify-self: center;
+        align-self: center;
+    } 
+
+.pa-2{
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+}
+
 @media(max-width: 1110px) {
     .triangle {
         width: 40vh;
-        clip-path: polygon(0% 0%, 100% 0%, 0% 50%);
+        /* clip-path: polygon(0% 0%, 100% 0%, 0% 50%); */
     }
+    .column {
+        width: auto;
+    } 
+
 }
 
-@media(max-width: 860px) {
-    .triangle {
-        width: 0vh;
-    }
-
-    .div2 {
-        width: 100vw;
-    }
-}
-
-@media(max-width: 425px) {
+@media(max-width: 700px) {
     form {
         margin-left: 20px;
         margin-right: 20px;
     }
+    .triangle {
+        display: none;
+    }
+    
+    .container {
+        grid-template-columns: 1fr;
+    }
+    .column {
+        width: auto;
+    } 
+    .card {
+        width:auto;
+    }
+    
 }
 
-@media(max-height: 700px) {}
 </style>
