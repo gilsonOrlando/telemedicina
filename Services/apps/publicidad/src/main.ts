bdd3a5eb574cd1b611e6c0c98a894f54928783fc
gilsonOrlando <gilson.quezada@unl.edu.ec>
2023-07-11 16:50:14 -0500

    aux

--- Services/apps/publicidad/src/main.ts
+++ Services/apps/publicidad/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { PublicidadModule } from './publicidad.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(PublicidadModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
