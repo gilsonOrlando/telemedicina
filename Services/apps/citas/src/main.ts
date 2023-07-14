04375591fa64df9af7ce6e34ec055f7ed9ecd1cc
gilsonOrlando <gilson.quezada@unl.edu.ec>
2023-07-14 15:16:48 -0500

    Documentacion citas

--- Services/apps/citas/src/main.ts
+++ Services/apps/citas/src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { CitasModule } from './citas.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(CitasModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
