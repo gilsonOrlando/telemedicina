97c7a313892cc09bde93c19f09900b3a9374c5c1
Jorge <jorge.poma@unl.edu.ec>
2023-07-19 17:34:15 -0500

    feat(publicidad_service): documentacion de swagger

--- Services/apps/api-gateway/src/publicidad/dtos/publicidad.dto.ts
+++ Services/apps/api-gateway/src/publicidad/dtos/publicidad.dto.ts
@@ -1,8 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+
 export class PublicidadDto {
-    descripcion: string;
-    imagen: string;
+  @ApiProperty({ description: 'Descripción de la publicidad', example: 'Aprovecha nuestra oferta especial' })
+  descripcion: string;
+
+  @ApiProperty({ description: 'URL de la imagen de la publicidad', example: 'https://ejemplo.com/imagen.jpg' })
+  imagen: string;
+
+  // Otras propiedades y validaciones para el DTO
 }
-export class updatePublicidadDto extends PartialType(
-    PublicidadDto,
-  ) {}
+
+export class UpdatePublicidadDto extends PartialType(PublicidadDto) {
+  // Puedes agregar propiedades adicionales específicas para la actualización si es necesario
+}
--- Services/apps/api-gateway/src/publicidad/publicidad-gateway.controller.ts
+++ Services/apps/api-gateway/src/publicidad/publicidad-gateway.controller.ts
@@ -1,25 +1,38 @@
-import { Body, Controller, Get, Post , Put, Param} from '@nestjs/common';
-import { PublicidadService } from './publicidad-gateway.service'
-import { PublicidadDto, updatePublicidadDto } from './dtos/publicidad.dto'
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Put, Param } from '@nestjs/common';
+import { PublicidadService } from './publicidad-gateway.service';
+import { PublicidadDto, UpdatePublicidadDto } from './dtos/publicidad.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
 
-@ApiTags("publicidad")
+@ApiTags('publicidad')
 @Controller()
 export class PublicidadController {
   constructor(private readonly publicidadService: PublicidadService) { }
 
   @Get('publicidad')
+  @ApiOperation({ summary: 'Obtener todas las publicidades' })
+  @ApiResponse({ status: 200, description: 'Éxito', type: PublicidadDto, isArray: true })
   getPublicidad() {
     return this.publicidadService.getPublicidad();
   }
+
   @Post('new-publicidad')
+  @ApiOperation({ summary: 'Crear nueva publicidad' })
+  @ApiBody({ type: PublicidadDto })
+  @ApiResponse({ status: 201, description: 'Creado', type: PublicidadDto })
   createPublicidad(@Body() publicidad: PublicidadDto) {
     return this.publicidadService.createPublicidad(publicidad);
   }
+
   @Put('update-publicidad/:id')
-   updatePublicidad(
-    @Param("id") id:string,
-    @Body() publicidad: updatePublicidadDto) {
+  @ApiOperation({ summary: 'Actualizar publicidad por ID' })
+  @ApiParam({ name: 'id', description: 'ID de la publicidad' })
+  @ApiBody({ type: UpdatePublicidadDto })
+  @ApiResponse({ status: 200, description: 'Éxito', type: PublicidadDto })
+  @ApiResponse({ status: 404, description: 'No encontrado' })
+  updatePublicidad(
+    @Param('id') id: string,
+    @Body() publicidad: UpdatePublicidadDto,
+  ) {
     return this.publicidadService.updatePublicidad(id, publicidad);
   }
 }
