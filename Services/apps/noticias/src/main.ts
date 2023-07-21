ee94b37f8612b5e0e854da8f88c3d3f4369c7607
Jhandry <jhandry.santorum@unl.edu.ec>
2023-07-21 18:42:49 -0500

    main de noticias

--- Services/apps/noticias/src/main.ts
+++ Services/apps/noticias/src/main.ts
@@ -1,30 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NoticiasModule } from './noticias.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import { RmqService } from '@app/common';
 async function bootstrap() {
   const app = await NestFactory.create(NoticiasModule);
-  const config = new DocumentBuilder()
-    .setTitle('Apis Noticia')
-    .setDescription('Documentación del modulo noticia')
-    .setVersion('1.0')
-    .addTag('noticias')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  app.useGlobalPipes(new ValidationPipe());
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
-}
-bootstrap();
-
-import { RmqService } from '@app/common';
-
   const rmqService = app.get<RmqService>(RmqService);
-  app.connectMicroservice(rmqService.getOptions('PUBLICIDAD_SERVICE'));
+  app.connectMicroservice(rmqService.getOptions('NOTICIAS_SERVICE'));
   await app.startAllMicroservices();
 }
 bootstrap();
